d8a32b4632df0bba30d4bbb8c55fd6499e354bb4
unknown <sjmayon1@utpl.edu.ec>
2023-12-06 15:51:18 -0500

    ejemplo4

--- ManejoCicloFor/src/paquete01/Ejemplo02.java
+++ ManejoCicloFor/src/paquete01/Ejemplo02.java
@@ -15,7 +15,7 @@ public class Ejemplo02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //* el i+=2 presenta valores que vallan de 2 en 2 
         for (int i = 1; i <= 10; i+=2) {
             System.out.printf("%d\n", i);
         }
--- ManejoCicloFor/src/paquete01/Ejemplo04.java
+++ ManejoCicloFor/src/paquete01/Ejemplo04.java
@@ -15,7 +15,7 @@ public class Ejemplo04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //* en el i-=2 sigifica que va decreciendo los valores de 2 en 2
         for (int i = 20; i >= 0; i-=2) {
             System.out.printf("%d\n", i);
         }
