31c81399fc62db7eebb15a7a82cd4bfbab187ca8
unknown <sjmayon1@utpl.edu.ec>
2023-12-06 16:47:54 -0500

    taller en clase

--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package paquete03;
-
+import java.util.Locale;
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -12,9 +13,27 @@ package paquete03;
 public class Ejemplo03 {
     
     public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) {
-            
+       Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int numero;
+        String cadena="";
+        System.out.println("ingresar limite");
+        numero=entrada.nextInt();
+        for (int i = 1; i <=numero; i++) {
+            entrada.nextLine();
+            System.out.println("ingrese el nombre del jugador");
+            String nombreJugador=entrada.nextLine();
+            System.out.println("posicion de campo de juego");
+            String posicionJugador=entrada.nextLine();
+            System.out.println("edad del jugador");
+            int edadJugador=entrada.nextInt();
+            System.out.println("estatura");
+            double estaturaJugador=entrada.nextDouble();
+            cadena=String.format("%s%d. %s-%s-,edad %d,estatura %.2f\n",
+                    cadena, i, nombreJugador, posicionJugador,
+                    edadJugador,
+                    estaturaJugador);
         }
+        System.out.printf("listado de jugadores\n%s", cadena);
     }
 }
